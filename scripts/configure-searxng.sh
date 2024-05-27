137bea0c74ee8723f51a0875d3b009cd5c5ffa3d
Jesús Daniel Colmenares Oviedo <DtxdF@disroot.org>
2024-05-27 07:42:33 -0400

    Use environment variables instead of dasel

--- scripts/configure-searxng.sh
+++ scripts/configure-searxng.sh
@@ -3,39 +3,41 @@
 # Environment:
 # - SEARXNG_DEBUG
 # - SEARXNG_PORT
-# - SEARXNG_BIND_ADDRESS (0.0.0.0)
-# - SEARXNG_SECRET_KEY
+# - SEARXNG_BIND_ADDRESS
+# - SEARXNG_SECRET
 # - SEARXNG_BASE_URL
 # - SEARXNG_REDIS_URL
 
 . /scripts/lib.subr
 
-SEARXNG_BIND_ADDRESS="${SEARXNG_BIND_ADDRESS:-0.0.0.0}"
+sysrc searxng_env="SEARXNG_SETTINGS_PATH=/usr/local/etc/searxng.yml" > /dev/null
 
 if [ -n "${SEARXNG_DEBUG}" ]; then
 	info "Configuring general.debug -> ${SEARXNG_DEBUG}"
-	put -t bool -v "${SEARXNG_DEBUG}" general.debug
+	sysrc searxng_env+="SEARXNG_DEBUG=${SEARXNG_DEBUG}" > /dev/null
 fi
 
 if [ -n "${SEARXNG_PORT}" ]; then
 	info "Configuring server.port -> ${SEARXNG_PORT}"
-	put -t int -v "${SEARXNG_PORT}" server.port
+	sysrc searxng_env+="SEARXNG_PORT=${SEARXNG_PORT}" > /dev/null
 fi
 
-info "Configuring server.bind_address -> ${SEARXNG_BIND_ADDRESS}"
-put -t string -v "${SEARXNG_BIND_ADDRESS}" server.bind_address
+if [ -n "${SEARXNG_BIND_ADDRESS}" ]; then
+	info "Configuring server.bind_address -> ${SEARXNG_BIND_ADDRESS}"
+	sysrc searxng_env+="SEARXNG_BIND_ADDRESS=${SEARXNG_BIND_ADDRESS}" > /dev/null
+fi
 
 if [ -n "${SEARXNG_SECRET}" ]; then
-	info "Configuring server.secret_key -> ${SEARXNG_SECRET_KEY}"
-	put -t string -v "${SEARXNG_SECRET_KEY}" server.secret_key
+	info "Configuring server.secret_key -> ${SEARXNG_SECRET}"
+	sysrc searxng_env+="SEARXNG_SECRET=${SEARXNG_SECRET}" > /dev/null
 fi
 
 if [ -n "${SEARXNG_BASE_URL}" ]; then
 	info "Configuring server.base_url -> ${SEARXNG_BASE_URL}"
-	put -t string -v "${SEARXNG_BASE_URL}" server.base_url
+	sysrc searxng_env+="SEARXNG_BASE_URL=${SEARXNG_BASE_URL}" > /dev/null
 fi
 
 if [ -n "${SEARXNG_REDIS_URL}" ]; then
 	info "Configuring redis.url -> ${SEARXNG_REDIS_URL}"
-	put -t string -v "${SEARXNG_REDIS_URL}" redis.url
+	sysrc searxng_env+="SEARXNG_REDIS_URL=${SEARXNG_REDIS_URL}" > /dev/null
 fi
